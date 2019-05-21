0ddb7e0c123a7c1d840ecefc92cdfc11f355fd09
Konstantin Dzuin <k.dzuin@corp.badoo.com>
2019-05-21 16:26:42 +0100

    remove initial component, we use splash screen for the very beginning

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,6 +4,12 @@ module.exports = {
     getSections() {
         return [
             {
+                name: 'Examples',
+                components: [
+                    // require('examples/my-typescript-component')
+                ]
+            },
+            {
                 name: 'UI',
                 components: [
                     require('components/component/component'),
@@ -12,44 +18,23 @@ module.exports = {
         ];
     },
 
+    getComponentRoots({ path }) {
+        const cwd = path.resolve(__dirname, '.');
+
+        return [
+            path.resolve(cwd, 'src'),
+        ];
+    },
+
     getWebpackConfig({ path }) {
         const cwd = path.resolve(__dirname, '.');
 
         return {
-            getComponentRoots() {
-                return [
-                    path.resolve(cwd, 'src')
-                ];
-            },
-
-            webpackConfig: {
-                resolve: {
-                    modules: [
-                        path.resolve(cwd, 'src/'),
-                        path.resolve(cwd, 'node_modules/'),
-                    ],
-                },
-                module: {
-                    rules: [
-                        {
-                            test: /\.(jpe?g|png|gif)$/i,
-                            use: [
-                                {
-                                    loader: 'file-loader',
-                                    options: {
-                                        hash: 'sha512',
-                                        digest: 'hex',
-                                        name: '[hash].[ext]',
-                                    },
-                                },
-                            ],
-                        },
-                        {
-                            test: /\.ttf$/,
-                            loader: 'file-loader',
-                        },
-                    ],
-                },
+            resolve: {
+                modules: [
+                    path.resolve(cwd, 'src/'),
+                    path.resolve(cwd, 'node_modules/'),
+                ],
             },
         };
     },
