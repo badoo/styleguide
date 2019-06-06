6c86e87f85031566f837684883cdaf62475c6d3a
Ivan Grekov <ivan.grekov@corp.badoo.com>
2019-06-06 23:35:02 +0100

    introduce ErrorBoundary for error catching during compilation of components

---
+++ src/components/error-boundary/error-boundary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './error-boundary.scss';
+
+export default class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasError: false,
+            error: null,
+            errorInfo: null,
+        };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ hasError: true, error, errorInfo });
+    }
+
+    render() {
+        const errorMessage = this.state.error ? this.state.error.toString() : null;
+        const errorInfoMessage = this.state.errorInfo ? this.state.errorInfo.componentStack : null;
+
+        return this.state.hasError ? (
+            <section className="error-boundary">
+                <h3 className="error-boundary__title">{errorMessage}</h3>
+                <p className="error-boundary__text">{errorInfoMessage}</p>
+            </section>
+        ) : (
+            this.props.children
+        );
+    }
+}
