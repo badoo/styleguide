094bb1750ee5171e86d0c6933559eaa79f6900e4
Ivan Grekov <ivan.grekov@magiclab.co>
2020-02-20 17:51:30 +0000

    divide ts-loader into several functions

--- loaders/ts-component.js
+++ loaders/ts-component.js
@@ -3,13 +3,68 @@ const reactDocsTS = require('react-docgen-typescript');
 const loaderUtils = require('loader-utils');
 const { isDebug } = require('../build-arguments');
 
+function setPropsFromDoc(doc) {
+    function setProps (types, key) {
+        const originalProp = doc.props[key];
+
+        types[key] = {
+            type: originalProp.type.name.replace('| undefined', ''),
+            description: originalProp.description,
+        };
+
+        // props with default value are not required
+        if (originalProp.defaultValue) {
+            types[key].defaultValue = originalProp.defaultValue.value;
+        } else {
+            types[key].required = originalProp.required;
+        }
+
+        return types;
+    }
+
+    return Object.keys(doc.props).reduce(setProps, {})
+}
+
+function setResults(tsConfigPath, resourcePath, source) {
+    try {
+        const docs = reactDocsTS.withCustomConfig(tsConfigPath).parse(resourcePath);
+
+        if (!docs[0]) {
+            return source;
+        }
+
+        const doc = docs[0];
+        const fileName = path.basename(resourcePath);
+        const fileNameWithoutPrefix = fileName.split('.').slice(0, -1).join('.');
+        const propTypes = doc.props ? setPropsFromDoc(doc) : null;
+
+        const meta = {
+            name: doc.displayName,
+            description: doc.description,
+            filePath: resourcePath,
+            fileName,
+            fileNameWithoutPrefix,
+            propTypes,
+        };
+
+        /* eslint-disable no-useless-escape */
+        return `${source}
+            export const __meta = ${JSON.stringify(meta)};
+            export const __dependencyResolver = require.context('./', true, /\.(j|t)sx?/);`;
+        /* eslint-enable no-useless-escape */
+    } catch (err) {
+        console.warn(path.basename(resourcePath), isDebug ? err : err.message);
+
+        return source;
+    }
+}
+
 module.exports = function(source) {
     if (this.cacheable) {
         this.cacheable();
     }
 
     const options = loaderUtils.getOptions(this);
-
     const componentRoots = options.componentRoots;
     const tsConfigPath = options.tsConfigPath;
 
@@ -22,58 +77,5 @@ module.exports = function(source) {
         return source;
     }
 
-    let results;
-
-    try {
-        const docs = reactDocsTS.withCustomConfig(tsConfigPath).parse(this.resourcePath);
-
-        if (!docs[0]) {
-            return source;
-        }
-
-        const doc = docs[0];
-        const fileName = path.basename(this.resourcePath);
-
-        const meta = {
-            name: doc.displayName,
-            description: doc.description,
-            filePath: this.resourcePath,
-            fileName,
-            fileNameWithoutPrefix: fileName
-                .split('.')
-                .slice(0, -1)
-                .join('.'),
-            propTypes: doc.props
-                ? Object.keys(doc.props).reduce(function(types, key) {
-                      const originalProp = doc.props[key];
-
-                      types[key] = {
-                          type: originalProp.type.name.replace('| undefined', ''),
-                          description: originalProp.description,
-                      };
-
-                      // props with default value are not required
-                      if (originalProp.defaultValue) {
-                          types[key].defaultValue = originalProp.defaultValue.value;
-                      } else {
-                          types[key].required = originalProp.required;
-                      }
-
-                      return types;
-                  }, {})
-                : null,
-        };
-
-        /* eslint-disable no-useless-escape */
-        results = `${source}
-            export const __meta = ${JSON.stringify(meta)};
-            export const __dependencyResolver = require.context('./', true, /\.(j|t)sx?/);`;
-        /* eslint-enable no-useless-escape */
-    } catch (err) {
-        console.warn(path.basename(this.resourcePath), isDebug ? err : err.message);
-
-        results = source;
-    }
-
-    return results;
+    return setResults(tsConfigPath, this.resourcePath, source);
 };
