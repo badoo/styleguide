742642e4befb50b301c082404d82f01b61b4f591
Ivan Grekov <ivan.grekov@magiclab.co>
2020-04-05 17:47:26 +0100

    add visual helpers

---
+++ visual-helpers/styleguide-group.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import View from './styleguide-view';
+
+const propTypes = {
+    children: PropTypes.node,
+    fullWidthItems: PropTypes.bool,
+};
+
+function StyleguideGroup(props) {
+    const { fullWidthItems, children } = props;
+
+    return (
+        <View
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+            }}
+        >
+            {React.Children.map(children, (child, key) => (
+                <View
+                    style={{
+                        width: fullWidthItems ? '100%' : 'auto',
+                        marginTop: 10,
+                        marginRight: !fullWidthItems ? 20 : 0,
+                        marginBottom: 10,
+                    }}
+                    key={`StyleguideGroup-0${key}`}
+                >
+                    {React.cloneElement(child)}
+                </View>
+            ))}
+        </View>
+    );
+}
+
+StyleguideGroup.propTypes = propTypes;
+
+export default StyleguideGroup;
---
+++ visual-helpers/styleguide-placeholder.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import View from './styleguide-view';
+import Text from './styleguide-text';
+
+const propTypes = {
+    width: PropTypes.string,
+    height: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    color: PropTypes.string,
+    placeholder: PropTypes.string,
+};
+
+function StyleguidePlaceholder(props) {
+    const { width, height, backgroundColor, color, placeholder } = props;
+
+    const placeholderText = placeholder || 'placeholder';
+
+    return (
+        <View
+            style={{
+                width: width || '100%',
+                height: height || '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: backgroundColor || '#fefefe',
+            }}
+        >
+            <Text
+                style={{
+                    fontSize: 8,
+                    fontFamily: 'monospace',
+                    color: color || '#777',
+                }}
+                ellipsizeMode={'middle'}
+                numberOfLines={1}
+            >
+                {placeholderText}
+            </Text>
+        </View>
+    );
+}
+
+StyleguidePlaceholder.propTypes = propTypes;
+
+export default StyleguidePlaceholder;
