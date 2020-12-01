f149b49bfcbe4c08854524d723da1ea2d0177678
Alexander Langolf <sasha.langolf@corp.badoo.com>
2020-12-01 03:07:52 +0000

    Grouped logo and search-field to give all extra space to navigation. Added scroll to nav list. Small cosmetic touches to search-field styles.

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -4,14 +4,12 @@ import styled from 'styled-components';
 const SidebarBlock = styled.aside`
     -webkit-font-smoothing: antialiased;
     position: relative;
-`;
-
-const SidebarContent = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
     background: #f0f0f0;
     border-right: 1px solid #f0f0f0;
     width: inherit;
-    overflow-y: auto;
-    min-height: 100%;
 `;
 
 const SidebarHeader = styled.div`
@@ -19,36 +17,45 @@ const SidebarHeader = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-bottom: 4px;
-    padding-top: 16px;
+    margin: 16px 0;
+    flex: 0 0 auto;
+`;
+
+const SidebarNavigation = styled.div`
+    flex: 1 1 auto;
+    overflow-y: auto;
+`;
+
+const SidebarSearchField = styled.div`
+    width: 100%;
+    padding: 0 16px;
 `;
 
 const SidebarLogo = styled.div`
     width: 32px;
     height: 32px;
-    margin-bottom: 4px;
+    margin-bottom: 8px;
 `;
 
 const Sidebar = props => {
     return (
         <SidebarBlock className={props.className}>
-            <SidebarContent>
-                <SidebarHeader>
-                    <SidebarLogo>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44">
-                            <rect width="44" height="44" fill="#7000E3" rx="3.9" />
-                            <path
-                                fill="#FF671B"
-                                d="M28 11.3c-2.3 0-4.6 1-6 3-1.4-2-3.7-3-6-3-4.3 0-7.8 3.3-7.8 7.6 0 6.5 9.3 15 13.8 15s13.8-8.5 13.8-15c0-4.4-3.5-7.7-7.8-7.7"
-                            />
-                        </svg>
-                    </SidebarLogo>
-                </SidebarHeader>
+            <SidebarHeader>
+                <SidebarLogo>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44">
+                        <rect width="44" height="44" fill="#7000E3" rx="3.9" />
+                        <path
+                            fill="#FF671B"
+                            d="M28 11.3c-2.3 0-4.6 1-6 3-1.4-2-3.7-3-6-3-4.3 0-7.8 3.3-7.8 7.6 0 6.5 9.3 15 13.8 15s13.8-8.5 13.8-15c0-4.4-3.5-7.7-7.8-7.7"
+                        />
+                    </svg>
+                </SidebarLogo>
 
-                {props.children}
-            </SidebarContent>
+                <SidebarSearchField>{props.searchField}</SidebarSearchField>
+            </SidebarHeader>
+
+            <SidebarNavigation>{props.navigation}</SidebarNavigation>
         </SidebarBlock>
     );
 };
-
 export default Sidebar;
